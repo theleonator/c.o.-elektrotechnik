86bd980995717794692f330065f028219ff9c144
theleonator <144523430+theleonator@users.noreply.github.com>
2026-04-18 18:24:01 +0200

    Wrap App component in a div with font-sans class

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,8 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <div className="font-sans">
+    <App />
+  </div>
+);
